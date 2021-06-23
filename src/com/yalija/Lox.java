35a37884a8c199135ef9e5dfcad6a1220c0a51a4
Danil Hendra S <danilhendrasr@gmail.com>
2021-06-23 08:17:21 +0700

    Make main function fully runnable

--- src/com/yalija/Lox.java
+++ src/com/yalija/Lox.java
@@ -1,7 +1,63 @@
 package com.yalija;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class Lox {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  static boolean hadError = false;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    String strings = Files.readString(Path.of(path), Charset.defaultCharset());
+    if (hadError)
+      System.exit(65);
+    run(strings);
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null)
+        break;
+      run(line);
+      hadError = false;
+    }
+  }
+
+  private static void run(String code) {
+    Scanner scanner = new Scanner(code);
+    List<Token> tokens = scanner.scanTokens();
+
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  protected static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
   }
 }
