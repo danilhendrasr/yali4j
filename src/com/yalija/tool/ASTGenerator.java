939d2e9127e0c8d762305302872831939e8a9ac8
Danil Hendra S <danilhendrasr@gmail.com>
2021-07-04 14:06:11 +0700

    implement visitor pattern in AST generator

--- src/com/yalija/tool/ASTGenerator.java
+++ src/com/yalija/tool/ASTGenerator.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class ASTGenerator {
   public static void main(String[] args) throws IOException {
@@ -34,6 +35,8 @@ public class ASTGenerator {
     writer.println();
     writer.println("abstract class " + baseName + " {");
 
+    defineVisitor(writer, baseName, types);
+
     // The AST Classes
     for (String type : types) {
       String className = type.split(":")[0].trim();
@@ -41,8 +44,24 @@ public class ASTGenerator {
       defineType(writer, baseName, className, fields);
     }
 
+    // The base accept() method
+    writer.println();
+    writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
     writer.println("}");
     writer.close();
+  }
+
+  private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+    writer.println("  interface Visitor<R> {");
+
+    for (String type : types) {
+      String typeName = type.split(":")[0].trim();
+      writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase(Locale.ROOT) + ");");
+    }
+
+    writer.println("  }");
+    writer.println();
   }
 
   private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -58,6 +77,13 @@ public class ASTGenerator {
       writer.println("      this." + name + " = " + name + ";");
     }
 
+    writer.println("    }");
+
+    writer.println();
+    writer.println("    @Override");
+    writer.println("    <R> R accept(Visitor<R> visitor) {");
+    writer.println("      return visitor.visit" + className + baseName +
+            "(this);");
     writer.println("    }");
 
     // Fields
