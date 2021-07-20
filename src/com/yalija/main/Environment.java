5ef960448e36db0f6d2363b8687361b13cda10cc
Danil Hendra S <danilhendrasr@gmail.com>
2021-07-20 11:04:05 +0700

    create Environment class
    Purpose:
    To hold every declared variable and their respective values

---
+++ src/com/yalija/main/Environment.java
@@ -0,0 +1,29 @@
+package com.yalija.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Environment {
+  private final Map<String, Object> values = new HashMap<>();
+
+  Object get(Token name) {
+    if (values.containsKey(name.lexeme)) {
+      return values.get(name.lexeme);
+    }
+
+    throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+  }
+
+  void assign(Token name, Object value) {
+    if (values.containsKey(name.lexeme)) {
+      values.put(name.lexeme, value);
+      return;
+    }
+
+    throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+  }
+
+  void define(String name, Object value) {
+    values.put(name, value);
+  }
+}
