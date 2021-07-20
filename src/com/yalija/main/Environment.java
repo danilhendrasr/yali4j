3c1bc0d0a479b69a316cca047efeb14df718203d
Danil Hendra S <danilhendrasr@gmail.com>
2021-07-20 11:27:32 +0700

    make Environment class to be able to be aware of its enclosing scope
    Purpose:
    Prepare for lexical scoping functionality

--- src/com/yalija/main/Environment.java
+++ src/com/yalija/main/Environment.java
@@ -4,12 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+  final Environment enclosingEnvironment;
   private final Map<String, Object> values = new HashMap<>();
+
+  Environment() {
+    enclosingEnvironment = null;
+  }
+
+  Environment(Environment enclosingEnvironment) {
+    this.enclosingEnvironment = enclosingEnvironment;
+  }
 
   Object get(Token name) {
     if (values.containsKey(name.lexeme)) {
       return values.get(name.lexeme);
     }
+
+    if (enclosingEnvironment != null) return enclosingEnvironment.get(name);
 
     throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
   }
@@ -17,6 +28,11 @@ public class Environment {
   void assign(Token name, Object value) {
     if (values.containsKey(name.lexeme)) {
       values.put(name.lexeme, value);
+      return;
+    }
+
+    if (enclosingEnvironment != null) {
+      enclosingEnvironment.assign(name, value);
       return;
     }
 
