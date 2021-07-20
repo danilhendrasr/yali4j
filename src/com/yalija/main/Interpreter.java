c70c79751aaec379bbb0fe54616df7596141400a
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2021-07-20 11:34:28 +0700

    Support lexical scoping (#9)
    * add Block subclass to Stmt class generator in ASTGenerator
    
    * add Block subclass to Stmt class
    
    * make Environment class to be able to be aware of its enclosing scope
    
    * make parser able to parse block statements and any other statements it contains
    
    * make interpreter able to execute statements inside of blocks

--- src/com/yalija/main/Interpreter.java
+++ src/com/yalija/main/Interpreter.java
@@ -71,6 +71,26 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     stmt.accept(this);
   }
 
+  private void executeBlock(List<Stmt> statements, Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
+
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
+    }
+  }
+
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(this.environment));
+    return null;
+  }
+
+
   @Override
   public Void visitExpressionStmt(Stmt.Expression stmt) {
     evaluate(stmt.expression);
