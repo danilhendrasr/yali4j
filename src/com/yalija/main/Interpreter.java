a4009cdb56225a6ffa706baf350f9708d7429d03
Danil Hendra S <danilhendrasr@gmail.com>
2021-07-28 08:05:01 +0700

    add clock() native function

--- src/com/yalija/main/Interpreter.java
+++ src/com/yalija/main/Interpreter.java
@@ -4,7 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-  private Environment environment = new Environment();
+  final Environment globals = new Environment();
+  private Environment environment = globals;
+
+  Interpreter() {
+    globals.define("clock", new LoxCallable() {
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter, List<Object> arguments) {
+        return (double) System.currentTimeMillis() / 1000;
+      }
+      
+      @Override
+      public String toString() {
+        return "<native fn>";
+      }
+    });
+  }
 
   void interpret(List<Stmt> statements) {
     try {
