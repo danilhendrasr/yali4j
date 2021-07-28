a8017d9a790bf022c91f12482608ed45aef3bced
Danil Hendra S <danilhendrasr@gmail.com>
2021-07-28 07:52:56 +0700

    enable parser to parse function calls

--- src/com/yalija/main/Parser.java
+++ src/com/yalija/main/Parser.java
@@ -245,7 +245,39 @@ public class Parser {
       return new Expr.Unary(operator, right);
     }
 
-    return primary();
+    return call();
+  }
+
+  private Expr call() {
+    Expr expr = primary();
+
+    while (true) {
+      if (match(TokenType.OPENING_PAREN)) {
+        expr = finishCall(expr);
+      } else {
+        break;
+      }
+    }
+
+    return expr;
+  }
+
+  private Expr finishCall(Expr callee) {
+    List<Expr> arguments = new ArrayList<>();
+
+    if (!check(TokenType.CLOSING_PAREN)) {
+      do {
+        if (arguments.size() >= 255) {
+          error(peek(), "Can't have more than 255 arguments.");
+        }
+        arguments.add(expression());
+      } while (match(TokenType.COMMA));
+    }
+
+    Token paren = consume(TokenType.CLOSING_PAREN, "Expect ')' after " +
+            "arguments.");
+
+    return new Expr.Call(callee, paren, arguments);
   }
 
   private Expr primary() {
