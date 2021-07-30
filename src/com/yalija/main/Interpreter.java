7518ee919fb63df1be72e8380bef671893708cdf
Danil Hendra S <danilhendrasr@gmail.com>
2021-07-30 23:22:30 +0700

    add support for closures

--- src/com/yalija/main/Interpreter.java
+++ src/com/yalija/main/Interpreter.java
@@ -132,7 +132,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitFunctionStmt(Stmt.Function stmt) {
-    LoxFunction function = new LoxFunction(stmt);
+    LoxFunction function = new LoxFunction(stmt, environment);
     environment.define(stmt.name.lexeme, function);
     return null;
   }
--- src/com/yalija/main/LoxFunction.java
+++ src/com/yalija/main/LoxFunction.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public class LoxFunction implements LoxCallable {
   private final Stmt.Function declaration;
+  private final Environment closure;
 
-  LoxFunction(Stmt.Function declaration) {
+  LoxFunction(Stmt.Function declaration, Environment closure) {
+    this.closure = closure;
     this.declaration = declaration;
   }
 
@@ -21,7 +23,7 @@ public class LoxFunction implements LoxCallable {
 
   @Override
   public Object call(Interpreter interpreter, List<Object> arguments) {
-    Environment environment = new Environment(interpreter.globals);
+    Environment environment = new Environment(closure);
     for (int i = 0; i < declaration.params.size(); i++) {
       environment.define(declaration.params.get(i).lexeme, arguments.get(i));
     }
@@ -31,7 +33,7 @@ public class LoxFunction implements LoxCallable {
     } catch (Return returnValue) {
       return returnValue.value;
     }
-    
+
     return null;
   }
 }
