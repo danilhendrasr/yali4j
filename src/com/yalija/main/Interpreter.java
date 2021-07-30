abfcd031bc5f6e20cb97c6331dde7becd2520166
Danil Hendra S <danilhendrasr@gmail.com>
2021-07-30 23:03:07 +0700

    make interpreter able to understand function declaration and return statement

--- src/com/yalija/main/Interpreter.java
+++ src/com/yalija/main/Interpreter.java
@@ -18,7 +18,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
       public Object call(Interpreter interpreter, List<Object> arguments) {
         return (double) System.currentTimeMillis() / 1000;
       }
-      
+
       @Override
       public String toString() {
         return "<native fn>";
@@ -104,7 +104,7 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     stmt.accept(this);
   }
 
-  private void executeBlock(List<Stmt> statements, Environment environment) {
+  public void executeBlock(List<Stmt> statements, Environment environment) {
     Environment previous = this.environment;
     try {
       this.environment = environment;
@@ -131,6 +131,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitFunctionStmt(Stmt.Function stmt) {
+    LoxFunction function = new LoxFunction(stmt);
+    environment.define(stmt.name.lexeme, function);
+    return null;
+  }
+
+  @Override
   public Void visitIfStmt(Stmt.If stmt) {
     if (isTruthy(evaluate(stmt.condition))) {
       execute(stmt.thenBranch);
@@ -146,6 +153,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
     return null;
+  }
+
+  @Override
+  public Void visitReturnStmt(Stmt.Return stmt) {
+    Object value = null;
+    if (stmt.value != null) value = evaluate(stmt.value);
+
+    throw new Return(value);
   }
 
   @Override
