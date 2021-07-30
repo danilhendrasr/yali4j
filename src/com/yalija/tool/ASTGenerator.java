2eafb9e329e7b3d640999da45e5bb18c7e5b4a4f
Danil Hendra S <danilhendrasr@gmail.com>
2021-07-30 23:00:37 +0700

    create Function and Return subclass in Stmt.java

--- src/com/yalija/tool/ASTGenerator.java
+++ src/com/yalija/tool/ASTGenerator.java
@@ -30,8 +30,10 @@ public class ASTGenerator {
     defineAST(outputDir, "Stmt", Arrays.asList(
             "Block      : List<Stmt> statements",
             "Expression : Expr expression",
+            "Function   : Token name, List<Token> params, List<Stmt> body",
             "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
             "Print      : Expr expression",
+            "Return     : Token keyword, Expr value",
             "Var        : Token name, Expr initializer",
             "While      : Expr condition, Stmt body"
     ));
