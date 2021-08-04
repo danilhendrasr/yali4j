ae53711019b9265e0b8fd6ce05805d962c7c67b5
Danil Hendra S <danilhendrasr@gmail.com>
2021-08-04 22:53:53 +0700

    enable interpreter to access a specific ancestor of the current environment

--- src/com/yalija/main/Environment.java
+++ src/com/yalija/main/Environment.java
@@ -42,4 +42,21 @@ public class Environment {
   void define(String name, Object value) {
     values.put(name, value);
   }
+
+  Environment ancestor(int distance) {
+    Environment environment = this;
+    for (int i = 0; i < distance; i++) {
+      environment = environment.enclosingEnvironment;
+    }
+
+    return environment;
+  }
+
+  Object getAt(int distance, String name) {
+    return ancestor(distance).values.get(name);
+  }
+
+  public void assignAt(int distance, Token name, Object value) {
+    ancestor(distance).values.put(name.lexeme, value);
+  }
 }
