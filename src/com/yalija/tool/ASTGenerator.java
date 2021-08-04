fd13b60a079b8e121d836cf60312cd9aa9e53dea
Danil Hendra S <danilhendrasr@gmail.com>
2021-08-04 23:31:44 +0700

    add Class subclass to Stmt class

--- src/com/yalija/tool/ASTGenerator.java
+++ src/com/yalija/tool/ASTGenerator.java
@@ -29,6 +29,7 @@ public class ASTGenerator {
 
     defineAST(outputDir, "Stmt", Arrays.asList(
             "Block      : List<Stmt> statements",
+            "Class      : Token name, List<Stmt.Function> methods",
             "Expression : Expr expression",
             "Function   : Token name, List<Token> params, List<Stmt> body",
             "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
