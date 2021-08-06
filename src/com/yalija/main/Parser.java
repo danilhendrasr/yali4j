c1d225b91de1f693d96e165f9f856f19679e8a07
Danil Hendra S <danilhendrasr@gmail.com>
2021-08-06 19:26:16 +0700

    make parser able to handle instance property assignment

--- src/com/yalija/main/Parser.java
+++ src/com/yalija/main/Parser.java
@@ -166,6 +166,9 @@ public class Parser {
       if (expr instanceof Expr.Variable) {
         Token name = ((Expr.Variable) expr).name;
         return new Expr.Assign(name, value);
+      } else if (expr instanceof Expr.Get) {
+        Expr.Get get = (Expr.Get) expr;
+        return new Expr.Set(get.object, get.name, value);
       }
 
       error(equals, "Invalid assignment target.");
@@ -303,6 +306,9 @@ public class Parser {
     while (true) {
       if (match(TokenType.OPENING_PAREN)) {
         expr = finishCall(expr);
+      } else if (match(TokenType.DOT)) {
+        Token name = consume(TokenType.IDENTIFIER, "Expect property name " +
+                "after '.'.");
       } else {
         break;
       }
