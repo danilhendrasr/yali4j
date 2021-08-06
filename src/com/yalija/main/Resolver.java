e94bd152938015ad8ffbc3c57ffdff4e317bc917
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2021-08-07 06:57:23 +0700

    Add support for instance methods execution (#22)
    - make LoxClass able to hold and find methods
    - make LoxInstance able to search for methods
    - fix simple parser bug that prevents creating instances
    - make resolver able to resolve methods
    - make interpreter able to handle getting and setting instance methods

--- src/com/yalija/main/Resolver.java
+++ src/com/yalija/main/Resolver.java
@@ -16,7 +16,8 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum FunctionType {
     NONE,
-    FUNCTION
+    FUNCTION,
+    METHOD
   }
 
   public void resolve(List<Stmt> statements) {
@@ -86,6 +87,12 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitClassStmt(Stmt.Class stmt) {
     declare(stmt.name);
     define(stmt.name);
+
+    for (Stmt.Function method : stmt.methods) {
+      FunctionType declaration = FunctionType.METHOD;
+      resolveFunction(method, declaration);
+    }
+
     return null;
   }
 
