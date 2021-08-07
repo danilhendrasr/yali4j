20f8717e67db623c95d421c1125d52e062f94be8
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2021-08-07 10:13:35 +0700

    Add support for "this" keyword (#23)
    - add This subclass to Expr class
    - add "this" binding support to LoxInstance
    - make parser able to handle this keywords
    - make resolver able to resolve this keywords
    - make interpreter able to handle this keywords

--- src/com/yalija/main/Expr.java
+++ src/com/yalija/main/Expr.java
@@ -12,6 +12,7 @@ abstract class Expr {
     R visitLiteralExpr(Literal expr);
     R visitLogicalExpr(Logical expr);
     R visitSetExpr(Set expr);
+    R visitThisExpr(This expr);
     R visitUnaryExpr(Unary expr);
     R visitVariableExpr(Variable expr);
   }
@@ -138,6 +139,19 @@ abstract class Expr {
     final Expr object;
     final Token name;
     final Expr value;
+  }
+
+  static class This extends Expr {
+    This (Token keyword) {
+      this.keyword = keyword;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitThisExpr(this);
+    }
+
+    final Token keyword;
   }
 
   static class Unary extends Expr {
--- src/com/yalija/main/Resolver.java
+++ src/com/yalija/main/Resolver.java
@@ -20,6 +20,13 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     METHOD
   }
 
+  private enum ClassType {
+    NONE,
+    CLASS
+  }
+
+  private ClassType currentClass = ClassType.NONE;
+
   public void resolve(List<Stmt> statements) {
     for (Stmt statement : statements) {
       resolve(statement);
@@ -85,14 +92,23 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitClassStmt(Stmt.Class stmt) {
+    ClassType enclosingClass = currentClass;
+    currentClass = ClassType.CLASS;
+
     declare(stmt.name);
     define(stmt.name);
+
+    beginScope();
+    scopes.peek().put("this", true);
 
     for (Stmt.Function method : stmt.methods) {
       FunctionType declaration = FunctionType.METHOD;
       resolveFunction(method, declaration);
     }
 
+    endScope();
+
+    currentClass = enclosingClass;
     return null;
   }
 
@@ -209,6 +225,17 @@ public class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   public Void visitSetExpr(Expr.Set expr) {
     resolve(expr.value);
     resolve(expr.object);
+    return null;
+  }
+
+  @Override
+  public Void visitThisExpr(Expr.This expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword, "Can't use 'this' outside of a class.");
+      return null;
+    }
+
+    resolveLocal(expr, expr.keyword);
     return null;
   }
 
