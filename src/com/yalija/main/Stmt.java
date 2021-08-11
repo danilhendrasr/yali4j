e73bb67b0b1d72ec851888ebd97795a86af43de9
Danil Hendra S <danilhendrasr@gmail.com>
2021-08-11 18:44:54 +0700

    modify Class subclass of Stmt class
    Added new parameter to store superclass.

--- src/com/yalija/main/Stmt.java
+++ src/com/yalija/main/Stmt.java
@@ -29,8 +29,9 @@ abstract class Stmt {
   }
 
   static class Class extends Stmt {
-    Class (Token name, List<Stmt.Function> methods) {
+    Class (Token name, Expr.Variable superclass, List<Stmt.Function> methods) {
       this.name = name;
+      this.superclass = superclass;
       this.methods = methods;
     }
 
@@ -40,6 +41,7 @@ abstract class Stmt {
     }
 
     final Token name;
+    final Expr.Variable superclass;
     final List<Stmt.Function> methods;
   }
 
